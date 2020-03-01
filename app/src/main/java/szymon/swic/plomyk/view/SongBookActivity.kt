fb0048219609761340187ba772bf42dfbd90e9bd
Szymon <szymonswic@gmail.com>
2020-03-01 20:34:41 +0100

    dependencies update

---
+++ app/src/main/java/szymon/swic/plomyk/view/SongBookActivity.kt
@@ -0,0 +1,13 @@
+package szymon.swic.plomyk.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import szymon.swic.plomyk.R
+
+class SongBookActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
