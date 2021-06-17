698d9fa8e5d8197221e0c6078aab1d582fe09d7f
Artur <artur.borowiec0@interia.pl>
2021-06-18 00:20:53 +0200

    Koin update

--- app/src/main/java/szymon/swic/plomyk/core/di/DatabaseModule.kt
+++ app/src/main/java/szymon/swic/plomyk/core/di/DatabaseModule.kt
@@ -1,6 +1,5 @@
 package szymon.swic.plomyk.core.di
 
-import androidx.room.Room
 import androidx.room.Room.databaseBuilder
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
