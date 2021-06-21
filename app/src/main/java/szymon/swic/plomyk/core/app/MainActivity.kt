0599de28427f3c7de773ea3c31b0e6be2d301688
Artur <artur.borowiec0@interia.pl>
2021-06-21 20:13:46 +0200

    Cleanup

--- app/src/main/java/szymon/swic/plomyk/core/app/MainActivity.kt
+++ app/src/main/java/szymon/swic/plomyk/core/app/MainActivity.kt
@@ -2,7 +2,6 @@ package szymon.swic.plomyk.core.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import szymon.swic.plomyk.R
 
@@ -15,12 +14,4 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp() =
         findNavController(R.id.nav_host_fragment).navigateUp()
-
-    fun replaceFragment(fragment: Fragment, addToBackStack: Boolean = false) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.nav_host_fragment, fragment)
-            .apply { if (addToBackStack) this.addToBackStack("") }
-            .commit()
-    }
 }
